6a586c4bf3517ec86d5d68b1426adf4be1520224
Stefano <stom4137@uni.sydney.edu.au>
2019-01-31 09:23:57 +1100

    changed __init__ file for qubit simulator folder

--- two_qubit_simulator/quantum_gates/__init__.py
+++ two_qubit_simulator/quantum_gates/__init__.py
@@ -2,3 +2,8 @@
 Initialise the quantum_gates module.
 Add import statements from auxilirary modules here.
 """
+from .phase_gate import Phase
+from .cnot import CNOT
+from .depolarising import Depolarising
+from .hadamard import Hadamard
+
